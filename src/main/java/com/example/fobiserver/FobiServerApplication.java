90e06da2896dc6488442e61f4c030a367281ae3a
ONE <31034469+choco-one@users.noreply.github.com>
2025-04-18 15:54:19 +0900

    init: init project

---
+++ src/main/java/com/example/fobiserver/FobiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fobiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FobiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FobiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fobiserver/FobiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fobiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FobiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
