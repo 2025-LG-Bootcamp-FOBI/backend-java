e062d13c014aa8de08d10c96a4a51d01f0741246
ONE <31034469+choco-one@users.noreply.github.com>
2025-04-23 15:31:47 +0900

    feat: update constant

--- src/main/java/com/example/fobiserver/constant/Constant.java
+++ src/main/java/com/example/fobiserver/constant/Constant.java
@@ -3,5 +3,5 @@ package com.example.fobiserver.constant;
 public class Constant {
     public static String PYTHON_PATH = "python3";
     public static String FOLDER_PATH = "shared";
-    public static String SCRIPT_PATH = "app/BasicParser.py";
+    public static String SCRIPT_PATH = "BasicParser.py";
 }
