a02e5810e47c0cf1a1190ee297f67c38a7f414f9
ONE <31034469+choco-one@users.noreply.github.com>
2025-04-24 09:25:49 +0900

    feat: add logging interceptor

--- src/main/java/com/example/fobiserver/configuration/WebConfiguration.java
+++ src/main/java/com/example/fobiserver/configuration/WebConfiguration.java
@@ -1,16 +1,28 @@
 package com.example.fobiserver.configuration;
 
+import com.example.fobiserver.util.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**");
     }
 }
